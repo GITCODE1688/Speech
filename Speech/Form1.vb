5546f8bccb0e7d9593bf7378e8385a01e9bc5f22
su <saxjesus@gmail.com>
2017-09-13 22:27:24 +0800

    修改1

--- Speech/Form1.vb
+++ Speech/Form1.vb
@@ -6,7 +6,7 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim MyVoice As New SpeechLib.SpVoice
         Dim strData As String
-        strData = StrConv(TextBox1.Text, VbStrConv.SimplifiedChinese, 2052)     ''''''test
+        strData = StrConv(TextBox1.Text, VbStrConv.SimplifiedChinese, 2052)     ''''''test  修改1
         MyVoice.Speak(strData)
     End Sub
 End Class
