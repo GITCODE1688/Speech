f08d81c54bd6d7765c8e97073a48a83352b88eda
su <saxjesus@gmail.com>
2017-09-13 22:46:54 +0800

    add load

--- Speech/Form1.vb
+++ Speech/Form1.vb
@@ -6,7 +6,12 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim MyVoice As New SpeechLib.SpVoice
         Dim strData As String
-        strData = StrConv(TextBox1.Text, VbStrConv.SimplifiedChinese, 2052)     '11111
+        strData = StrConv(TextBox1.Text, VbStrConv.SimplifiedChinese, 2052)     '22222
         MyVoice.Speak(strData)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = "南無密斯佛陀"
+        Button1_Click(Nothing, Nothing)
     End Sub
 End Class
